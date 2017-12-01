82f3b011392987cade601047b68ca5dda05c45d2
Corey Stockton <coreystockton4c@gmail.com>
2017-12-01 18:00:08 -0500

    Created header file for drive related functions

---
+++ include/drive.h
@@ -0,0 +1,15 @@
+#ifndef _DRIVE_H_
+#define _DRIVE_H_
+
+//List of all functions to be declared
+
+//beginner list
+void setDriveForTime(int left, int right, int time);
+
+//normal list
+void setDrive(int left, int right);
+void stopDrive();
+
+
+
+#endif _DRIVE_H_ //end declaration if
