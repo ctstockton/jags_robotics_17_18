74d5c124c7d5e2c07a1a26f283d61cd7872e987d
Corey Stockton <coreystockton4c@gmail.com>
2017-12-04 13:32:53 -0500

    Create lift.h

---
+++ include/lift.h
@@ -0,0 +1,14 @@
+#ifndef _LIFT_H_
+#define _LIFT_H_
+
+//List of all function to be declared
+
+//beginner list
+void setLiftForTime(int lift, int time);
+
+//normal list
+void setLift(int lift);
+void stopLift();
+
+
+#endif _LIFT_H_ //end declaration if
