c7d63f20d6badcea4168c0755e0ab97907077705
Corey Stockton <coreystockton4c@gmail.com>
2017-12-07 20:32:23 -0500

    Added lift and drive functions

--- src/drive.c
+++ src/drive.c
@@ -1,3 +1,32 @@
 #include "main.h" //to include API and drive.h
 
 //functions from drive.h go in here
+
+void stopDrive()
+{
+  //stop all motors for drive
+  motorStop(LEFT_FRONT_MOTOR);
+  motorStop(LEFT_BACK_MOTOR);
+  motorStop(RIGHT_BACK_MOTOR);
+  motorStop(RIGHT_FRONT_MOTOR);
+}
+
+void setDriveForTime(int leftPower, int rightPower, int time)
+{
+  //set left and right drive for some power for some time then stop
+  motorSet(LEFT_FRONT_MOTOR, leftPower);
+  motorSet(LEFT_BACK_MOTOR, leftPower);
+  motorSet(RIGHT_BACK_MOTOR, rightPower);
+  motorSet(RIGHT_FRONT_MOTOR, rightPower);
+  delay(time);
+  stopDrive();
+}
+
+void setDrive(int leftPower, int rightPower)
+{
+  //set all drive motors to some power
+  motorSet(LEFT_FRONT_MOTOR, leftPower);
+  motorSet(LEFT_BACK_MOTOR, leftPower);
+  motorSet(RIGHT_BACK_MOTOR, rightPower);
+  motorSet(RIGHT_FRONT_MOTOR, rightPower);
+}
--- src/lift.c
+++ src/lift.c
@@ -1,3 +1,25 @@
 #include "main.h" //to include API and lift.h
 
 //functions from lift.h go in here
+void stopLift()
+{
+  //set all lift motors
+  motorStop(LEFT_LIFT_MOTOR);
+  motorStop(RIGHT_LIFT_MOTOR);
+}
+
+void setLiftForTime(int power, int time)
+{
+  //set all motors at some lift power and for some time
+  motorSet(LEFT_LIFT_MOTOR, power);
+  motorSet(RIGHT_LIFT_MOTOR, power);
+  delay(time);
+  stopLift();
+}
+
+void setLift(int power)
+{
+  //set all motors to some lift power
+  motorSet(LEFT_LIFT_MOTOR, power);
+  motorSet(RIGHT_LIFT_MOTOR, power);
+}
