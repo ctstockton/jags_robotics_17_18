63bce30aed97bae1fa3a61915c0b912dc0dc8434
Corey Stockton <coreystockton4c@gmail.com>
2017-12-09 14:04:54 -0500

    LCD start up

--- src/lcd.c
+++ src/lcd.c
@@ -1,14 +1,27 @@
 #include "main.h" //to include API and lcd.h
 
 //function for running the LCD autonomous menu error backlight flash
-void runLCDMenu()
+void runLCDMenu()//called as a thread in init.c
 {
-
+  lcdClear(uart1);//just in case
+  lcdSetText(uart1, 1, "    LCD Menu    ");
+  lcdSetText(uart1, 2, "Starting Up");
+  delay(250);
+  lcdSetText(uart1, 2, "Starting Up.");
+  delay(250);
+  lcdSetText(uart1, 2, "Starting Up..");
+  delay(250);
+  lcdSetText(uart1, 2, "Starting Up...");
+  delay(250);
+  lcdSetText(uart1, 2, "Starting Up....");
+  delay(250);
+  lcdSetText(uart1, 2, "Starting Up.....");
+  
 }
 
 bool backLight;
 
-void flashLCDBacklight()
+void flashLCDBacklight()//called as a thread when the robot need attention
 {
   if(!backLight)//if backlight is off, turn on
   {
