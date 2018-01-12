11217da590a536642f379ace6ce792742c2347e1
Corey Stockton <coreystockton4c@gmail.com>
2018-01-12 11:28:28 -0500

    Lift PID & Task

--- include/lift.h
+++ include/lift.h
@@ -1,6 +1,16 @@
 #ifndef _LIFT_H_
 #define _LIFT_H_
 
+#include "main.h"
+
+//declare task name
+TaskHandle _lift;
+
+//declare preset heights
+#define LIFT_TO_BOTTOM 927
+#define LIFT_TO_TOP 1850
+
+int LIFT_TARGET;
 //List of all function to be declared
 
 //beginner list
@@ -10,5 +20,7 @@ void setLiftForTime(int power, int time);
 void setLift(int power);
 void stopLift();
 
+//lift task function
+void liftTask(void * parameters);
 
 #endif //_LIFT_H_ end declaration if
--- src/lift.c
+++ src/lift.c
@@ -12,7 +12,7 @@ void setLiftForTime(int power, int time)
 {
   //set all motors at some lift power and for some time
   motorSet(LEFT_LIFT_MOTOR, power);
-  motorSet(RIGHT_LIFT_MOTOR, power);
+  motorSet(RIGHT_LIFT_MOTOR, -power);
   delay(time);
   stopLift();
 }
@@ -21,5 +21,31 @@ void setLift(int power)
 {
   //set all motors to some lift power
   motorSet(LEFT_LIFT_MOTOR, power);
-  motorSet(RIGHT_LIFT_MOTOR, power);
+  motorSet(RIGHT_LIFT_MOTOR, -power);
+}
+
+void liftTask(void * parameters)
+{
+  float np = 0.40;
+  float ni = 0.017;
+  float nd = 0.27;
+  int error = 0;
+  float derivative = 0;
+  int previousError = 0;
+  float errorSum = 0;
+  int motorPower = 0;
+
+  while(true)
+  {
+    error = LIFT_TARGET - analogRead(LEFT_LIFT_POTENTIOMETER);
+    derivative = abs(error - previousError)/0.02;//ticks per sec
+    motorPower = (error*np+errorSum*ni+derivative*nd);
+    setLift(motorPower);
+    previousError = error;
+    errorSum += error*0.02;//0.02 is for the Riemann Sum
+
+    //lcdSetText(uart2, 1, "Err: Int: Deriv:");//for debug
+    //lcdPrint(uart2, 2, "%3d %3.1f %2.1f",error,errorSum,derivative);//for debug
+    delay(20);
+  }
 }
