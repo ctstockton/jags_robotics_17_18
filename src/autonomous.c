fc20c34ac895935469fbfaaa119896276e34230c
naat123 <35389427+naat123@users.noreply.github.com>
2018-01-13 01:38:32 -0500

    started auto

--- src/autonomous.c
+++ src/autonomous.c
@@ -12,5 +12,13 @@ void LeftAutonomous1()
 
 void RightAutonomous1()
 {
+int Quad_Right = 0;
+imeReset(RIGHT_DRIVE_QUAD_TOP);
+while(Quad_Right < 400)
+{
+Quad_Right = encoderGet(RBDriveEncoder);
+motorSet(2,220);
+motorSet(3,220);
+}
 
 }
