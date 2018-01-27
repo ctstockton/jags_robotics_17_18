3ce14cb426bacd67dbbb5e5106fb6573bc3a819e
Corey Stockton <coreystockton4c@gmail.com>
2018-01-26 19:49:07 -0500

    Drive functions

--- include/drive.h
+++ include/drive.h
@@ -2,15 +2,20 @@
 #define _DRIVE_H_
 
 //List of all functions to be declared
+#define ENCODER_TOL 30
 
 //beginner list
 void setDriveForTime(int left, int right, int time);
 void setStrafeForTime(int front, int back, int time);
+
 //normal list
 void setDrive(int left, int right);
 void setStrafe(int front, int back);
 void stopDrive();
 
-
+//drive functions
+void driveStraight(int distance, int power, int timeout);
+void strafeStraight(int distance, int power, int timeout);
+void tankTurn(int distance, int power, int timeout);
 
 #endif //_DRIVE_H_ end declaration if
--- src/drive.c
+++ src/drive.c
@@ -16,8 +16,18 @@ void setDriveForTime(int leftPower, int rightPower, int time)
   //set left and right drive for some power for some time then stop
   motorSet(LEFT_FRONT_MOTOR, leftPower);
   motorSet(LEFT_BACK_MOTOR, leftPower);
-  motorSet(RIGHT_BACK_MOTOR, rightPower);
-  motorSet(RIGHT_FRONT_MOTOR, rightPower);
+  motorSet(RIGHT_BACK_MOTOR, -rightPower);
+  motorSet(RIGHT_FRONT_MOTOR, -rightPower);
+  delay(time);
+  stopDrive();
+}
+
+void setStrafeForTime(int frontPower, int backPower, int time)
+{
+  motorSet(LEFT_FRONT_MOTOR, frontPower);
+  motorSet(LEFT_BACK_MOTOR, -backPower);
+  motorSet(RIGHT_BACK_MOTOR, -backPower);
+  motorSet(RIGHT_FRONT_MOTOR, frontPower);
   delay(time);
   stopDrive();
 }
@@ -27,6 +37,66 @@ void setDrive(int leftPower, int rightPower)
   //set all drive motors to some power
   motorSet(LEFT_FRONT_MOTOR, leftPower);
   motorSet(LEFT_BACK_MOTOR, leftPower);
-  motorSet(RIGHT_BACK_MOTOR, rightPower);
-  motorSet(RIGHT_FRONT_MOTOR, rightPower);
+  motorSet(RIGHT_BACK_MOTOR, -rightPower);
+  motorSet(RIGHT_FRONT_MOTOR, -rightPower);
+}
+
+void setStrafe(int frontPower, int backPower)
+{
+  motorSet(LEFT_FRONT_MOTOR, frontPower);
+  motorSet(LEFT_BACK_MOTOR, -backPower);
+  motorSet(RIGHT_BACK_MOTOR, -backPower);
+  motorSet(RIGHT_FRONT_MOTOR, frontPower);
+}
+
+void driveStraight(int distance, int power, int timeout)
+{
+  if(!AUTONOMOUS_ERROR)
+  {
+    int beginning = millis();
+    int end = beginning + timeout;
+
+    encoderReset(LFDriveEncoder);
+    encoderReset(RBDriveEncoder);
+
+    while((abs(encoderGet(LFDriveEncoder))<distance && abs(encoderGet(RBDriveEncoder))<distance) && (int)millis() < end)
+    {
+      if(abs(encoderGet(LFDriveEncoder)<abs(encoderGet(RBDriveEncoder))))
+        setDrive(power, power*0.95);
+      else if(abs(encoderGet(LFDriveEncoder)>abs(encoderGet(RBDriveEncoder))))
+        setDrive(power*0.95, power);
+      else
+        setDrive(power, power);
+      delay(20);
+    }
+    stopDrive();
+    if((int)millis() > end)
+      AUTONOMOUS_ERROR = true;
+  }
+}
+
+void strafeStraight(int distance, int power, int timeout)
+{
+  if(!AUTONOMOUS_ERROR)
+  {
+    int beginning = millis();
+    int end = beginning + timeout;
+
+    encoderReset(LFDriveEncoder);
+    encoderReset(RBDriveEncoder);
+
+    while((abs(encoderGet(LFDriveEncoder))<distance && abs(encoderGet(RBDriveEncoder))<distance) || (int)millis() < end)
+    {
+      if(abs(encoderGet(LFDriveEncoder)<abs(encoderGet(RBDriveEncoder))))
+        setStrafe(power, power*0.95);
+      else if(abs(encoderGet(LFDriveEncoder)>abs(encoderGet(RBDriveEncoder))))
+        setStrafe(power*0.95, power);
+      else
+        setStrafe(power, power);
+      delay(20);
+    }
+    stopDrive();
+    if((int)millis() < end)
+      AUTONOMOUS_ERROR = true;
+  }
 }
